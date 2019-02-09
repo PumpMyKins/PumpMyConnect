9b5f397fb52cbd2dfb6446bd60c91892824175e5
Clem-Fern-Dev <clement.coussieu@gmail.com>
2019-02-09 22:30:02 +0100

    Save

--- bungee/src/main/java/fr/pmc/PumpMyConnectBungee.java
+++ bungee/src/main/java/fr/pmc/PumpMyConnectBungee.java
@@ -1,6 +1,9 @@
 package fr.pmc;
 
 import fr.pmc.core.utils.Logging;
+import fr.pmc.servers.Server;
+import fr.pmc.servers.ServersManager;
+import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.plugin.Plugin;
 
 public class PumpMyConnectBungee extends Plugin {
@@ -9,6 +12,11 @@ public class PumpMyConnectBungee extends Plugin {
 	public void onEnable() {
 		
 		Logging.setLogger(getLogger());
+		
+		ServersManager serverManager = new ServersManager();		
+		for (ServerInfo iterable_element : getProxy().getServers().values()) {
+			serverManager.addServer(new Server(iterable_element));			
+		}
 		
 	}
 	
--- bungee/src/main/java/fr/pmc/servers/ServersManager.java
+++ bungee/src/main/java/fr/pmc/servers/ServersManager.java
@@ -1,16 +1,26 @@
 package fr.pmc.servers;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.logging.Level;
 
 import fr.pmc.core.utils.Logging;
+import net.md_5.bungee.config.Configuration;
+import net.md_5.bungee.config.YamlConfiguration;
 
 public class ServersManager extends Logging{
 	
 	private HashMap<String, Server> servers;
+	
+	private File configFile;
+	private Configuration config;
 
 	public ServersManager() {
 		
 		this.servers = new HashMap<String, Server>();
+		
+		
 		
 	}
 	
@@ -20,14 +30,50 @@ public class ServersManager extends Logging{
 			return;
 		}
 		
+		String serverPath = "servers." + server.getName();
+		
+		if(this.getConfig().contains(serverPath)) {
+			
+			server.setDisplayName(this.config.getString(serverPath + ".displayname"));
+			server.setAutoCheck(this.config.getBoolean(serverPath + ".autocheck"));
+			
+		}else {
+			
+			this.config.set(serverPath + ".displayname", server.getDisplayName());
+			this.config.set(serverPath + ".autocheck", server.isAutoCheck());
+			
+			try {
+				
+				this.saveConfig();
+				getLogger().info("PMC configuration: " + server.getName() + " added in configuration file");
+				
+			} catch (IOException e) {
+				
+				getLogger().log(Level.SEVERE, "PMC configuration: saving error when adding server[" + server.getName() + "] !");
+				e.printStackTrace();
+				return;
+				
+			}
+			
+		}
+		
 		this.servers.put(server.getName(), server);
 		getLogger().info("PMC: " + server.getName() + " added in ServerManager.");
 		
-		
 	}
 	
+	private void saveConfig() throws IOException {
+		
+		YamlConfiguration.getProvider(YamlConfiguration.class).save(this.config, this.configFile);
+				
+	}
+
 	public HashMap<String, Server> getServers() {
 		return servers;
+	}
+	
+	public Configuration getConfig() {		
+		return this.config;		
 	}
 	
 }
