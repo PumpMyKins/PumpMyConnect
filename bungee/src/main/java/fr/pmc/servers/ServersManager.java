6e8027df882e28d34f50ec83cd715b3cb8879cf3
Clem-Fern-Dev <clement.coussieu@gmail.com>
2019-02-09 17:19:13 +0100

    Update name & method in ServerManager class

--- bungee/src/main/java/fr/pmc/servers/ServersManager.java
+++ bungee/src/main/java/fr/pmc/servers/ServersManager.java
@@ -5,11 +5,25 @@ import java.util.HashMap;
 public class ServersManager {
 
 	private HashMap<String, Server> servers;
-	
+
 	public ServersManager() {
 		
 		this.servers = new HashMap<String, Server>();
 		
+	}
+	
+	public void addServer(Server server) {
+		
+		if(this.servers.containsKey(server.getName())) {
+			return;
+		}
+		
+		this.servers.put(server.getName(), server);
+		
+	}
+	
+	public HashMap<String, Server> getServers() {
+		return servers;
 	}
 	
 }
--- core/src/main/java/fr/pmc/core/servers/ConnectManager.java
+++
@@ -1,9 +0,0 @@
-package fr.pmc.servers;
-
-import java.util.HashMap;
-
-public class ConnectManager {
-
-	//private HashMap<String, >
-	
-}
