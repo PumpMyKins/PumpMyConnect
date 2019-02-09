c46cafa6fb8556cf9ea89f7494f138d44e7490e4
Clem-Fern-Dev <clement.coussieu@gmail.com>
2019-02-09 17:35:21 +0100

    Add onEnable method in PumpMyConnectBungee class & Logging class init

--- bungee/src/main/java/fr/pmc/PumpMyConnectBungee.java
+++ bungee/src/main/java/fr/pmc/PumpMyConnectBungee.java
@@ -1,5 +1,15 @@
 package fr.pmc;
 
-public class PumpMyConnectBungee {
+import fr.pmc.core.utils.Logging;
+import net.md_5.bungee.api.plugin.Plugin;
 
+public class PumpMyConnectBungee extends Plugin {
+
+	@Override
+	public void onEnable() {
+		
+		Logging.setLogger(getLogger());
+		
+	}
+	
 }
