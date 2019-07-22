becca6489dd3703de63f1584efefcadea4c1d092
Clem-Fern <clemfern456@gmail.com>
2019-07-22 23:51:38 +0200

    Update MainPumpMyBConnect class

--- bungee/src/main/java/fr/pmc/MainPumpMyBConnect.java
+++ bungee/src/main/java/fr/pmc/MainPumpMyBConnect.java
@@ -1,41 +1,34 @@
 package fr.pmc;
 
 import java.io.IOException;
-import java.util.Map.Entry;
-import java.util.logging.Level;
+import java.util.logging.Logger;
 
-import fr.pmc.core.utils.Logging;
-import fr.pmc.servers.Server;
 import fr.pmc.servers.ServersManager;
-import net.md_5.bungee.api.config.ServerInfo;
 import net.md_5.bungee.api.plugin.Plugin;
 
-public class PumpMyConnectBungee extends Plugin {
+public class MainPumpMyBConnect extends Plugin {
+
+	public static Logger LOGGER;
+	public static ServersManager SERVERS_MANAGER;
 
 	@Override
 	public void onEnable() {
 		
-		Logging.setLogger(getLogger());
+		LOGGER = this.getLogger();
 		
-        if (!getDataFolder().exists()) {
-            getDataFolder().mkdir();
-        }
-		
-		ServersManager serverManager;
 		try {
-			serverManager = new ServersManager(this);
 			
-			for (ServerInfo server : getProxy().getServers().values()) {
-				serverManager.addServer(new Server(server));			
-			}
+			ServersManager manager = new ServersManager(this);
+			manager.init();
+			SERVERS_MANAGER = manager;
 			
-		} catch (IOException e) {			
-			getLogger().log(Level.SEVERE, "PMC configuration: initializing servers.yml file error !");
-			e.printStackTrace();			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			this.getProxy().stop();
 			return;
-		}	
+		}
 		
-
 		
 	}
 	
