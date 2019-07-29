97342e645a46593f9256d3168cd55639136ff6ed
Clem-Fern <clemfern456@gmail.com>
2019-07-30 01:02:42 +0200

    Update Connector class

--- bungee/src/main/java/fr/pmc/connectors/Connector.java
+++ bungee/src/main/java/fr/pmc/connectors/Connector.java
@@ -1,5 +1,26 @@
 package fr.pmc.connectors;
 
-public class Connector {
+import fr.pmc.utils.IConnectable;
+import net.md_5.bungee.config.Configuration;
+
+public class Connector implements IConnectable{
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void writeConfiguration(Configuration conf) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void readConfiguration(Configuration conf) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
