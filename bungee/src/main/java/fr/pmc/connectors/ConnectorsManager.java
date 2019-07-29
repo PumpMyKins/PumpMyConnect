9b7e4a224603c446f3b8778ca77279cfcbf19e14
Clem-Fern <clemfern456@gmail.com>
2019-07-30 01:06:58 +0200

    Update imports in COnnectorsManager class

--- bungee/src/main/java/fr/pmc/connectors/ConnectorsManager.java
+++ bungee/src/main/java/fr/pmc/connectors/ConnectorsManager.java
@@ -1,5 +1,18 @@
 package fr.pmc.connectors;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map.Entry;
+
+import fr.pmc.MainPumpMyBConnect;
+import fr.pmc.servers.Server;
+import fr.pmc.servers.ServersManagerConfig;
+import fr.pmc.servers.reloader.ServersReloaderManager;
+import net.md_5.bungee.api.config.ListenerInfo;
+import net.md_5.bungee.api.config.ServerInfo;
+
 public class ConnectorsManager {
 
 }
