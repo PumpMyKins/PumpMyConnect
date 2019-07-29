4f2a6a03ade70702ea43b98c427cad82f338906c
Clem-Fern <clemfern456@gmail.com>
2019-07-30 01:07:43 +0200

    Update ConnectorsManager class

--- bungee/src/main/java/fr/pmc/connectors/ConnectorsManager.java
+++ bungee/src/main/java/fr/pmc/connectors/ConnectorsManager.java
@@ -1,18 +1,58 @@
 package fr.pmc.connectors;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map.Entry;
-
 import fr.pmc.MainPumpMyBConnect;
-import fr.pmc.servers.Server;
-import fr.pmc.servers.ServersManagerConfig;
-import fr.pmc.servers.reloader.ServersReloaderManager;
-import net.md_5.bungee.api.config.ListenerInfo;
-import net.md_5.bungee.api.config.ServerInfo;
 
 public class ConnectorsManager {
 
+	private HashMap<String, Connector> connectors;
+	private ConnectorsManagerConfig managerConfig;
+
+	public ConnectorsManager(MainPumpMyBConnect pumpMyConnectBungee) throws IOException {
+		
+		this.connectors = new HashMap<>();
+		this.managerConfig = new ConnectorsManagerConfig(pumpMyConnectBungee);
+		
+		List<String> connectorsFiles = this.managerConfig.getConnectorsFiles();
+		MainPumpMyBConnect.LOGGER.info("[ServersManager] " + connectorsFiles.size() + " connectors configuration file(s) found.");
+		
+		
+		/*new ArrayList<>(pumpMyConnectBungee.getProxy().getConfigurationAdapter().getListeners()).get(0).getForcedHosts()
+		
+		for (Entry<String, ServerInfo> entry : pumpMyConnectBungee.getProxy().getConfigurationAdapter().getListeners()) {
+			
+			Server server = new Server(entry.getValue());
+			if(serverFiles.contains(server.getName())) {
+				this.managerConfig.loadFileInConnector(server);
+				MainPumpMyBConnect.LOGGER.info("[ConnectorsManager]=> Server[" + server.getName() + "] successfully loaded File");
+			}else {
+				this.managerConfig.exportConnectorInFile(server);
+				MainPumpMyBConnect.LOGGER.info("[ConnectorsManager]=> Server[" + server.getName() + "] successfully created File");
+			}			
+			this.connectors.put(entry.getKey(), server);
+			
+		}
+		
+		for (String string : serverFiles) {
+			
+			if(this.connectors.containsKey(string)) {
+				continue;
+			}
+			
+			MainPumpMyBConnect.LOGGER.warning("[ServersManagerConfig]=> Server[" + string + "] unused file found.");
+			
+		}*/
+		
+	}
+
+	public ConnectorsManagerConfig getManagerConfig() {
+		return managerConfig;
+	}
+
+	public HashMap<String,Connector> getConnectors() {
+		return connectors;
+	}
+	
 }
