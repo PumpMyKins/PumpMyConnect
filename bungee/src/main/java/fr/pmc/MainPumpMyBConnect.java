bf0dbe6e4ad57dc4d32e2a681c2e9380ca27dbd9
Clem-Fern <clemfern456@gmail.com>
2019-07-30 01:05:25 +0200

    Updating log messages & adding Connectors manager field in Main class

--- bungee/src/main/java/fr/pmc/MainPumpMyBConnect.java
+++ bungee/src/main/java/fr/pmc/MainPumpMyBConnect.java
@@ -3,6 +3,7 @@ package fr.pmc;
 import java.io.IOException;
 import java.util.logging.Logger;
 
+import fr.pmc.connectors.ConnectorsManager;
 import fr.pmc.servers.ServersManager;
 import net.md_5.bungee.api.plugin.Plugin;
 
@@ -10,33 +11,48 @@ public class MainPumpMyBConnect extends Plugin {
 
 	public static Logger LOGGER;
 	public static ServersManager SERVERS_MANAGER;
-	
+	public static ConnectorsManager CONNECTORS_MANAGER;
+
 	public static ConfigManager CONFIG_MANAGER;
 
 	@Override
 	public void onEnable() {
-		
+
 		LOGGER = this.getLogger();
-		
+
 		try {
-			CONFIG_MANAGER = new ConfigManager(this);			
+			
+			CONFIG_MANAGER = new ConfigManager(this);	
+			LOGGER.info("Configurations manager successfully loaded !");
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 			this.getProxy().stop();
 			return;
 		}
-		
+
 		try {
-			
+
 			SERVERS_MANAGER = new ServersManager(this);
+			LOGGER.info("Servers manager successfully loaded !");
 			
 		} catch (IOException e) {
 			e.printStackTrace();
 			this.getProxy().stop();
 			return;
 		}
-		
-		
+
+		try {
+
+			CONNECTORS_MANAGER = new ConnectorsManager(this);
+			LOGGER.info("Connectors manager successfully loaded !");
+
+		} catch (IOException e) {
+			e.printStackTrace();
+			this.getProxy().stop();
+			return;
+		}
+
 	}
-	
+
 }
